1df3f945a4b5e5c4633d356d095de800d1986644
brokenwifi9 <81181897+Mainakdey1@users.noreply.github.com>
2024-07-23 15:57:20 +0530

    Commits

---
+++ Interface/raw_inp.py
@@ -0,0 +1,13 @@
+import numpy
+import cv2
+import numpy as np
+import imageio.v3
+import time
+import sys
+start_time=time.time()
+
+sys.set_int_max_str_digits(10000000)
+
+imgcolor=imageio.v3.imread(r"C:\Users\chestor\Desktop\galaxy.jpg")
+
+#use imgcolor for further analysis
