795ad26973bf2866f81568c730b0688de646e16c
brokenwifi9 <81181897+Mainakdey1@users.noreply.github.com>
2024-07-24 16:44:51 +0530

    Added support for raw text encryption

--- Interface/raw_inp.py
+++ Interface/raw_inp.py
@@ -1,13 +1,55 @@
-import numpy
-import cv2
-import numpy as np
+
 import imageio.v3
 import time
 import sys
+
+
+
+
+
+
+
+
 start_time=time.time()
 
 sys.set_int_max_str_digits(10000000)
+def img_inp_method(path):
+    imgcolor=imageio.v3.imread(path) # please add "r" in front of the path in case of input errors.
+    rows, cols, rgb=imgcolor.shape
 
-imgcolor=imageio.v3.imread(r"C:\Users\chestor\Desktop\galaxy.jpg")
+    R=[]
+    G=[]
+    B=[]
+    for i in range(rows):
+        for j in range(cols):
+            B.append(imgcolor[i,j,0])
+            G.append(imgcolor[i,j,1])
+            R.append(imgcolor[i,j,2])
+
+
+    return R+G+B
+
+def text_inp_method(raw_text):
+    res=[]
+    lt=len(raw_text)
+    for i in range(lt):
+        res+=[ord(raw_text[i]),]
+
+
+    return res
+
+print("1. Image Encryption\n2. Plain-Text Encryption")
+stat_inp=str(input())
+
+if stat_inp=="1":
+ 
+    cnv_list=img_inp_method(str(input("Please enter the path of your image")))
+
+elif stat_inp=="2":
+
+    cnv_list=text_inp_method(str(input("Please enter your text")))
+
+
+print(cnv_list)
 
 #use imgcolor for further analysis
